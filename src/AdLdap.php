31ab6f8df253ea252507f5ae79667f431cd1cd34
Sylvio Neto <dmsylvio@gmail.com>
2019-11-13 15:49:36 -0300

    correcao

---
+++ src/AdLdap.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Time Ago plugin for Craft CMS 3.x
+ *
+ * Time ago twig filter
+ *
+ * @link      https://github.com/dmsylvio
+ * @copyright Copyright (c) 2019 Sylvio Neto
+ */
+namespace dmsylvio\adldap;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+
+use yii\base\Event;
+
+
+/**
+ * Class AdLdap
+ *
+ * @author    Sylvio Neto
+ * @package   AdLdap
+ * @since     1.0.0
+ *
+ */
+class AdLdap extends \craft\base\Plugin
+{
+  public function init()
+  {
+    parent::init();
+
+    // Custom initialization code goes here...
+  }
+}
